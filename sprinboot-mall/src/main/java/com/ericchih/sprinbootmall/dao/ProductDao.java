c1d894a802b5f4cf88ca3938709ac5d19ea841d5
Eric <s22695415@gmail.com>
2022-11-17 11:12:41 +0800

    商品功能-實作修改商品

--- sprinboot-mall/src/main/java/com/ericchih/sprinbootmall/dao/ProductDao.java
+++ sprinboot-mall/src/main/java/com/ericchih/sprinbootmall/dao/ProductDao.java
@@ -8,4 +8,9 @@ public interface ProductDao {
     Product getProductById (Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
+
+    void updateProduct (Integer productId,ProductRequest productRequest);
+
+
+
 }
