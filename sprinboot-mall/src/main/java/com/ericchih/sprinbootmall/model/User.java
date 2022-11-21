0108d5f1e6c5afab5ca6079731c1e470e0b25e71
Eric <s22695415@gmail.com>
2022-11-21 17:43:02 +0800

    帳號功能-註冊新帳號

--- sprinboot-mall/src/main/java/com/ericchih/sprinbootmall/model/User.java
+++ sprinboot-mall/src/main/java/com/ericchih/sprinbootmall/model/User.java
@@ -1,11 +1,17 @@
 package com.ericchih.sprinbootmall.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.Date;
 
 public class User {
 
     private Integer userId;
+
+
     private String email;
+    @JsonIgnore
     private String password;
 
     public Integer getUserId() {
--- sprinboot-mall/src/main/java/com/ericchih/sprinbootmall/service/Impl/UserServiceImpl.java
+++ sprinboot-mall/src/main/java/com/ericchih/sprinbootmall/service/Impl/UserServiceImpl.java
@@ -4,12 +4,17 @@ import com.ericchih.sprinbootmall.dao.UserDao;
 import com.ericchih.sprinbootmall.dto.UserRegisterRequest;
 import com.ericchih.sprinbootmall.model.User;
 import com.ericchih.sprinbootmall.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class UserServiceImpl implements UserService {
 
+    private final static Logger log= LoggerFactory.getLogger(UserServiceImpl.class);
     @Autowired
     private UserDao userDao;
 
@@ -21,7 +26,15 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
+        User user= userDao.getUserByEmail(userRegisterRequest.getEmail());
 
+        //檢查註冊的 email
+        if (user != null){
+            log.warn("該 email {} 已經被註冊",userRegisterRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        //創建帳號
         return userDao.createUser(userRegisterRequest);
     }
 }
